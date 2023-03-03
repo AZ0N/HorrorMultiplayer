647928837ba8f1922a309d8d03a0803869618c93
Mads Nielsen <madscwn@gmail.com>
2023-03-03 11:18:22 +0100

    Moved movement and looking to new input system

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -5,7 +5,7 @@ public class PlayerNetwork : NetworkBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float mouseSensitivity = 30f;
 
     [Header("Camera & Audio")]
     [SerializeField] private Camera playerCamera;
@@ -16,7 +16,9 @@ public class PlayerNetwork : NetworkBehaviour
 
     private float rotationX = 0;
     private float rotationY = 0;
-    private Vector2 moveDir;
+
+    private InputActions inputActions;
+    private InputActions.PlayerActions playerActions;
 
     private void Start()
     {
@@ -26,21 +28,19 @@ public class PlayerNetwork : NetworkBehaviour
         audioListener.enabled = true;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        inputActions = new InputActions();
+        playerActions = inputActions.Player;
+        inputActions.Player.Enable();
     }
 
     private void Update()
     {
         if (!IsOwner) return;
 
-        moveDir = Vector2.zero;
-        if (Input.GetKey(KeyCode.W)) moveDir.y += 1;
-        if (Input.GetKey(KeyCode.S)) moveDir.y -= 1;
-        if (Input.GetKey(KeyCode.A)) moveDir.x -= 1;
-        if (Input.GetKey(KeyCode.D)) moveDir.x += 1;
-
-        // Normalize movement direction
+        // Movement
+        Vector2 moveDir = playerActions.Move.ReadValue<Vector2>();
         moveDir = moveDir.normalized;
-        // Move the player in the players forward direction based on the moveDir
         transform.position += (transform.forward * moveDir.y + transform.right * moveDir.x) * moveSpeed * Time.deltaTime;
 
         // Cursor Locking
@@ -54,15 +54,19 @@ public class PlayerNetwork : NetworkBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+        
         // Only do rotation if the cursor is locked
         if (Cursor.lockState != CursorLockMode.Locked) return;
 
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        rotationX -= mouseY; // X rotation is controlled by the mouse movement on the Y axis
-        rotationY += mouseX; // Y rotation is controlled by the mouse movement on the X axis
-        rotationX = Mathf.Clamp(rotationX, -90f, 90f);  // Clamp the X rotation to prevent the camera from flipping
+        // Rotation/Looking
+        Vector2 lookInput = playerActions.Look.ReadValue<Vector2>();
+        lookInput *= Time.deltaTime * mouseSensitivity;
+        
+        rotationX -= lookInput.y;
+        rotationY += lookInput.x;
 
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);  // Clamp the X rotation to prevent the camera from flipping
+        
         // Apply the rotation to the player and camera 
         transform.localRotation = Quaternion.Euler(0, rotationY, 0);
         playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
